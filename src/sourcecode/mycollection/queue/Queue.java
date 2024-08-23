0981dfa2e750390371c473aafa32961cfb0f8aab
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-08-23 22:29:35 +0530

    Adding Valid Comment to Queue Interface.

--- src/sourcecode/mycollection/queue/Queue.java
+++ src/sourcecode/mycollection/queue/Queue.java
@@ -1,9 +1,28 @@
 package sourcecode.mycollection.queue;
 
+/**
+ * Queue support FIFO (First In First Out)
+ */
 public interface Queue<E> {
+
+    /**
+     * Inserts the specified element into the Queue at the Tail Position
+     * @param data the element to add
+     */
     void enQueue(E data);
-    /*return the deQueued element for the Queue.
-    * if isQueue isEmpty() return null */
+
+    /**
+     * Delete/Remove the element from the head position of the Queue
+     * @return element data (E) that is going to be removed.
+     * @return null, if Queue isEmpty (size=0)
+    */
     E deQueue();
+
+    /**
+     * Check the emptiness of the  Queue empty(size=0)
+     * @return true,  if Queue is empty (not contains any element(size == 0))
+     * @return false, if Queue contains at-least one element(size >0).
+     */
     boolean isEmpty();
+
 }
