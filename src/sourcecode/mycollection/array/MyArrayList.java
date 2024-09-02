c3b22f81c1c6f7f8abb45f0421a28aa7489d068f
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-09-03 01:30:24 +0530

    MyArrayList Implementation

---
+++ src/sourcecode/mycollection/array/MyArrayList.java
@@ -0,0 +1,160 @@
+package sourcecode.mycollection.array;
+
+import java.util.Arrays;
+
+/**
+ * @noinspection unchecked
+ */
+public class MyArrayList<E> implements List<E> {
+
+    private Object[] dataArray;
+    private int size;
+    private static int DEFAULT_ARRAY_SIZE = 10;
+
+
+    public MyArrayList() {
+        dataArray = new Object[DEFAULT_ARRAY_SIZE];
+    }
+
+
+    @Override
+    public void add(E element) {
+        if (size < DEFAULT_ARRAY_SIZE) {
+            dataArray[size] = element;
+            size++;
+        } else {
+            throw new IndexOutOfBoundsException("Array Already Full");
+
+
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private boolean isValidIndex(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Valid Index Range is :[0," + size() + "):, Actual index=" + index);
+        }
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        if (isValidIndex(index)) {
+            return (E) dataArray[index];
+        }
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (isValidIndex(index)) {
+            dataArray[index] = element;
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null) {
+            for (Object object : dataArray) {
+                if (object == null) {
+                    return true;
+                }
+            }
+        } else {
+            for (Object object : dataArray) {
+                if (o.equals(object)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        E oldData = null;
+        if (isValidIndex(index)) {
+            oldData = (E) dataArray[index];
+            dataArray[index] = element;
+        }
+        return oldData;
+    }
+
+    @Override
+    public E remove(int index) {
+        E removedData = null;
+        if (isValidIndex(index)) {
+            removedData = (E) dataArray[index];
+            fastRemove(index);
+            size--;
+        }
+        return removedData;
+    }
+
+    // Shift the element towards right after deletion.
+    private void fastRemove(int index){
+        System.arraycopy(dataArray, index + 1, dataArray, index, size - index);
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size(); i++) {
+            dataArray[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder dataArrayToString = new StringBuilder();
+        dataArrayToString.append("[");
+        for (int i = 0; i < size; i++) {
+            if (i == 0) {
+                dataArrayToString.append(this.dataArray[i]);
+            } else {
+                dataArrayToString.append(", ").append(this.dataArray[i]);
+            }
+        }
+        dataArrayToString.append("]");
+        return "MyArrayList\n{\n" +
+                "  dataArray=" + dataArrayToString +
+                ",\n  size=" + size() +
+                "\n}\n";
+    }
+
+
+    public static void main(String[] args) {
+        MyArrayList<Integer> integerMyArrayList = new MyArrayList<>();
+        //integerMyArrayList.add(0);
+        integerMyArrayList.add(10);
+        integerMyArrayList.add(20);
+        integerMyArrayList.add(30);
+        integerMyArrayList.add(40);
+        integerMyArrayList.add(50);
+        integerMyArrayList.add(60);
+        integerMyArrayList.add(70);
+        integerMyArrayList.add(80);
+        integerMyArrayList.add(90);
+
+        System.out.println(integerMyArrayList);
+        System.out.println(integerMyArrayList.get(8));
+        integerMyArrayList.add(8, 45);
+        System.out.println(integerMyArrayList.contains(45));
+        System.out.println(integerMyArrayList);
+        System.out.println("remove ::"+integerMyArrayList.remove(5));
+        System.out.println(integerMyArrayList);
+        integerMyArrayList.clear();
+        System.out.println(integerMyArrayList);
+
+
+    }
+}
