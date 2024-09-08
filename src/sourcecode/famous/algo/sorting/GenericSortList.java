5e2ff5442d9cbdffaa3f10a118d8226bde53064d
Vishal Singh Yadav <vishal637yadav@gmail.com>
2024-09-08 19:26:36 +0530

    GenericSortList can take any parameter and sort using Bubble Sort

---
+++ src/sourcecode/famous/algo/sorting/GenericSortList.java
@@ -0,0 +1,113 @@
+package sourcecode.famous.algo.sorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GenericSortList<T extends Comparable<T>> {
+
+    List<T> dataList;
+
+    public GenericSortList() {
+        dataList = new ArrayList<>();
+    }
+
+    public void add( T element){
+        dataList.add(element);
+    }
+
+    //Sort Using Collections.sort() method
+    public void sortUsingCollectionSort(){
+        Collections.sort(this.dataList);
+    }
+
+    //Bubble sort Implementation
+    public void bubbleSort() {
+        int size = dataList.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (dataList.get(j).compareTo(dataList.get(j + 1)) > 0) {
+                    T oldValueAtJthIndex = dataList.set(j, dataList.get(j + 1));
+                    dataList.set(j + 1, oldValueAtJthIndex);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "GenericSortList{" +
+                "dataList=" + dataList +
+                '}';
+    }
+
+    static void createStringArray() {
+        System.out.println("------------createStringList--start--------------------");
+
+        GenericSortList<String> stringArray = new GenericSortList<>();
+        stringArray.dataList.add("aa");
+        stringArray.dataList.add("a");
+        stringArray.dataList.add("zz");
+        stringArray.dataList.add("147");
+        stringArray.dataList.add("b");
+        stringArray.dataList.add("cbb");
+        stringArray.dataList.add("ax");
+        stringArray.dataList.add("axd");
+        stringArray.dataList.add("asxd");
+
+        System.out.println("before bubbleSort()         stringArray::" + stringArray);
+        stringArray.bubbleSort();
+        System.out.println("after  bubbleSort()         stringArray::" + stringArray);
+        Collections.sort(stringArray.dataList);
+        System.out.println("after  Collections.sort()   stringArray::" + stringArray);
+        System.out.println("------------createStringList--ends--------------------");
+
+    }
+
+    public static void createIntegerArray() {
+        System.out.println("------------createIntegerList--start--------------------");
+        GenericSortList<Integer> integerArray = new GenericSortList<>();
+        integerArray.dataList.add(65);
+        integerArray.dataList.add(55);
+        integerArray.dataList.add(5);
+        integerArray.dataList.add(6);
+        integerArray.dataList.add(5);
+        integerArray.dataList.add(365);
+        integerArray.dataList.add(1);
+        integerArray.dataList.add(34);
+
+        System.out.println("before bubbleSort()         integerArray::" + integerArray);
+        integerArray.bubbleSort();
+        System.out.println("After  bubbleSort()         integerArray::" + integerArray);
+        Collections.sort(integerArray.dataList);
+        System.out.println("After  Collections.sort()   integerArray::" + integerArray);
+        System.out.println("------------createIntegerList--ends--------------------");
+
+    }
+
+    static void createStudentSortedList(){
+        System.out.println("------------createStudentSortedList--start--------------------");
+        GenericSortList<Student> studentList = new GenericSortList<>();
+        studentList.add(new Student(2001,"Amar","RIMS School",10));
+        studentList.add(new Student(2002,"Amar","DPS School",10));
+        studentList.add(new Student(2003,"Ajay","RIMS School",10));
+        studentList.add(new Student(4001,"Vijay","RIMS School",9));
+        studentList.add(new Student(4001,"Vijay","Apex School",10));
+        studentList.add(new Student(6002,"Deva-anand","RIMS School",10));
+
+        System.out.println("before bubbleSort()         studentList::" + studentList);
+        studentList.bubbleSort();
+        System.out.println("after  bubbleSort()         studentList::" + studentList);
+        studentList.sortUsingCollectionSort();
+        System.out.println("after  Collections.sort()   studentList::" + studentList);
+        System.out.println("------------createStudentSortedList--ends--------------------");
+    }
+
+    public static void main(String[] args) {
+
+        createStringArray();
+        createIntegerArray();
+        createStudentSortedList();
+
+    }
+}
---
+++ src/sourcecode/famous/algo/sorting/Student.java
@@ -0,0 +1,47 @@
+package sourcecode.famous.algo.sorting;
+
+public class Student implements Comparable<Student> {
+
+    private int id;
+    private String name;
+    private String college;
+    private int standard;
+
+    public Student(int id, String name, String college, int standard) {
+        this.id = id;
+        this.name = name;
+        this.college = college;
+        this.standard = standard;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", college='" + college + '\'' +
+                ", standard=" + standard +
+                '}';
+    }
+
+    /*
+    1. standard asc ;
+    2. name asc;
+    3. college asc;
+     */
+    @Override
+    public int compareTo(Student student) {
+        if (this == student || this.equals(student)) {
+            return 0;
+        }
+
+        int standardComparisonResult = this.standard - student.standard;
+        int nameComparisonResult = this.name.compareTo(student.name);
+        int collegeComparisonResult = this.college.compareTo(student.college);
+        return standardComparisonResult == 0 ? nameComparisonResult == 0 ? collegeComparisonResult : nameComparisonResult : standardComparisonResult;
+    }
+
+}
